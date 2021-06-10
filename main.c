a9c7f316b1c04d75293c7e519a69914e059c8a52
yavor2 <85577690+yavor2@users.noreply.github.com>
2021-06-10 21:05:11 +0300

    Fix main.c ; added basic menu for the questions

--- main.c
+++ main.c
@@ -1,87 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <string.h>
+#include<time.h>
 
-int size_of_file();
 
 char fname[]={"questions.dat"};
 
 struct question_t{
-	char question[200];
-	char a[30];
-	char b[30];
-	char c[30];
-	char d[30];
+	char question[300];
+	char a[50];
+	char b[50];
+	char c[50];
+	char d[50];
 	unsigned int answer;	
 	unsigned int difficulty;
 };
 
-struct question_t rquestion_f(){
-    
-    FILE *fp;
-    fp=fopen(fname,"rb");
-    struct question_t q;
-    int sizef = size_of_file();
-    int rng = rand() % sizef;
-
-    int j;
-    for(j = 0; j != rng;j++){
-        fread(&q,sizeof(q),1,fp);
-    };
-    return q;
-}
-
-//saving random 10 questions
-struct question_t *sv_10q(){
-    FILE *fp;
-    
-
-    struct question_t *fq;
-    struct question_t arr[10];
-    
-    fp=fopen(fname,"rb");
-    
-    
-    
-    for(int i = 0; i < 10 ; ){
-        
-        
-        struct question_t rq = rquestion_f();
-        
-        if(i < 3){
-            if(rq.difficulty == 0){
-                arr[i] = rq;
-                i++;
-            };
-        };
-                
-        
-        if(i >= 3 && i <= 5){
-            if(rq.difficulty == 1){
-                arr[i] = rq;
-                i++;
-            };
-        };
-        if(i >= 6 && i <= 9){
-            if(rq.difficulty == 2){
-                arr[i] = rq;
-                i++;
-            };
-        };
-    };
-        
-    fq = &arr[0];
-    
-    fclose(fp);
-    
-    return fq;
-        
-
-    
-}
 
 int size_of_file(){
-    
     FILE *fp;
     int size = 0;
     struct question_t q;
@@ -98,31 +34,94 @@ int size_of_file(){
     return size - 1;
 }
 
-void display()
-{
+struct question_t rquestion_f(){
+    FILE *fp;
+    fp=fopen(fname,"rb");
+    struct question_t q;
+    int sizef = size_of_file();
+    int rng = rand() % sizef;
+
+    int j;
+    for(j = 0; j <= rng;j++){
+        fread(&q,sizeof(q),1,fp);
+    };
+    return q;
+}
+
+void add_to_arr(struct question_t **arr, struct question_t q,int *size){
+    *size+=1;
+    *arr = (struct question_t*)realloc(*arr, (*size)*sizeof(struct question_t));
+	(*arr)[*size-1] = q;
+
+}
+
+int exists(struct question_t *arr, struct question_t q, int size){
+	for(int i=0;i<size;i++){
+		if(!strcmp(arr[i].question, q.question)) return 1;
+	}
+	return 0;
+}
+
+//saving random 10 questions
+struct question_t *sv_10q(){
+    FILE *fp;
+    struct question_t *arr = (struct question_t*)malloc(sizeof(struct question_t));
+    int size = 0;
+    fp=fopen(fname,"rb");
+    
+    
+    for(int i = 0; i < 10;){
+        struct question_t rq = rquestion_f();   
+        if(exists(arr,rq,size)) continue;
+        if(i < 3){
+            if(rq.difficulty == 0){
+                add_to_arr(&arr,rq,&size);
+                i++;
+            };
+        };  
+        if(i >= 3 && i <= 5){
+            if(rq.difficulty == 1){
+                add_to_arr(&arr,rq,&size);
+                i++;
+            };
+        };
+        if(i >= 6 && i <= 9){
+            if(rq.difficulty == 2){
+                add_to_arr(&arr,rq,&size);
+                i++;
+            };
+        };
+    };
+    fclose(fp);
+    return arr;
+}
+
+
+void display_all(){
 	FILE *fp;
 	struct question_t q;
-
+	int i=1;
 	fp=fopen(fname,"rb");
 
 	printf("\n========================================================\n\n");
 	printf("\t\t All question details\n\n");
 	printf("========================================================\n\n");
 
-	printf("Question\tA\tB\tC\tD\n\n");
-
+	printf("Question\t\t\tA\t\t\tB\t\t\tC\t\t\tD\tNUMBER\n\n");
+	
 	while(1){
 		fread(&q,sizeof(q),1,fp);
 
 		if(feof(fp)){
 			break;
 		}
-		printf("%s\t",q.question);
-		printf("%s\t",q.a);
-		printf("%s\t",q.b);
-		printf("%s\t",q.c);
-		printf("%s\t\n\n",q.d);
-
+		printf("%s\t\t",q.question);
+		printf("%s\t\t",q.a);
+		printf("%s\t\t",q.b);
+		printf("%s\t\t",q.c);
+		printf("%s\t\t",q.d);
+		printf("%d\t\n\n",i);
+		i++;
 	}
 	printf("========================================================\n\n");
 	fclose(fp);
@@ -157,12 +156,14 @@ void append_ui(){
 	printf("\nEnter correct answer (1-4):");			
 	scanf("%u",&q.answer);
 	// todo: if diff != (1-3)
-	printf("\nEnter difficulty (1-10):");			
+	printf("\nEnter difficulty (0-2):");			
 	scanf("%u",&q.difficulty);
 
 	fwrite(&q,sizeof(q),1,fp);
 	fclose(fp);
+	printf("\n\n");
 }
+
 void append(struct question_t *questions, int size){
 	FILE *fp;
 	
@@ -175,19 +176,43 @@ void append(struct question_t *questions, int size){
 }
 
 
+//-------------------------------------------------------------
 
+void start_game(){
+	struct question_t *questions = sv_10q(); //size=10
+	int round=1;
+	int ans;
+	for(int i=0;i<10;i++,round++){
+		printf("[---Question for round %d---]\n",round);
+		printf("%s\n",questions[i].question);
+		
+		printf("1. %s\n", questions[i].a);
+		printf("2. %s\n", questions[i].b);
+		printf("3. %s\n", questions[i].c);
+		printf("4. %s\n", questions[i].d);
+		
+		printf("\nEnter the correct answer: ");
+		scanf("%d",&ans);
+		if(ans != questions[i].answer){
+			printf("\nYou lost the game.\nYou played for [%d] rounds\n", round);
+			printf("\n\n");	
+			return;
+		}			
+		
+	}
+	printf("Congratulations! You won!\n\n");
+}
 
 int main(){
-    
-
+	srand(time(0));
 	struct question_t questions[] = {
-	{"VUPROS1", "OTGOVOR1A", "ORGOVOR1B", "OTGOVOR1G", "ORGOVOR1D", 2,0},
-	{"VUPROS2", "OTGOVOR2A", "ORGOVOR2B", "OTGOVOR2G", "ORGOVOR2D", 1,1},	
+	{"VUPROS1", "OTGOVOR1AAAAAAAAA", "ORGOVOR1B", "OTGOVOR1G", "ORGOVOR1D", 2,0},
+	{"VUPROS2", "OfdasGOVOR2A", "ORGOfasVOR2B", "OTGOVOfasR2G", "ORGOVOR2D", 1,1},	
 	{"VUPROS3", "OTGOVOR3A", "ORGOVOR3B", "OTGOVOR3G", "ORGOVOR3D", 4,2},
-	{"VUPROS4", "OTGOVOR4A", "ORGOVOR4B", "OTGOVOR4G", "ORGOVOR4D", 3,1},	
-	{"VUPROS5", "OTGOVOR5A", "ORGOVOR5B", "OTGOVOR5G", "ORGOVOR5D", 2,2},
+	{"VUPROS4", "OTGOVOR4A", "ORGOVOR4B", "OTGOVOR4fadsG", "ORGOVOR4D", 3,1},	
+	{"VUPROSS5", "OTGOVOR5A", "ORGOVOR5B", "OTGfasOVOR5G", "ORGOVOR5D", 2,2},
 	{"VUPROS6", "OTGOVOR6A", "ORGOVOR6B", "OTGOVOR6G", "ORGOVOR6D", 1,0},	
-	{"VUPROS7", "OTGOVOR7A", "ORGOVOR7B", "OTGOVOR7G", "ORGOVOR7D", 4,2},
+	{"VUPROS7", "OTGOVOR7A", "ORGOVOR7B", "OTGfdsOVOR7G", "ORGOVOR7D", 4,2},
 	{"VUPROS8", "OTGOVOR8A", "ORGOVOR8B", "OTGOVOR8G", "ORGOVOR8D", 1,1},	
 	{"VUPROS9", "OTGOVOR9A", "ORGOVOR9B", "OTGOVOR9G", "ORGOVOR9D", 2,2},
 	{"VUPROS10", "OTGOVOR10A", "ORGOVOR10B", "OTGOVOR10G", "ORGOVOR10D", 3,2},	
@@ -196,20 +221,35 @@ int main(){
     {"VUPROS13", "OTGOVOR13A", "ORGOVOR13B", "OTGOVOR13G", "ORGOVOR13D", 1,1},
 	};
 	append(questions, sizeof(questions)/sizeof(questions[0]));
-	display();
+	//display_all();
     
-    struct question_t *questions10 = sv_10q();
     
-    for(int i = 0; i < 10;i++){
-        printf("%s\n",(questions10+i)->question);
+    while(1){
+    	int o;
+    	printf("1. Start game\n");
+    	printf("2. Add a question\n");
+    	printf("3. Display all available questions\n");
+    	printf("0. Exit\n\n");
+    	printf("Select option: ");    
+    	scanf("%d",&o);
+    	getchar();
+    	printf("\n");
+    	switch(o){
+			case 1: start_game(); 
+			break;
+			
+			case 2: append_ui();
+			break;
+			
+			case 3: display_all();
+			break;
+						
+			case 0: exit(0);
+			break;
+    	}
     }
-
-
+    
 }
-
-
-
-
 
 
 
