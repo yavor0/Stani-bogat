449dcf5c23eb23e5ea201824407bb41a08c6c1cb
AlexGechovski <48589371+AlexGechovski@users.noreply.github.com>
2021-06-10 16:27:12 +0300

    Working array with 10 random questions.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include <string.h>
 
+int size_of_file();
+
 char fname[]={"questions.dat"};
 
 struct question_t{
@@ -14,24 +16,86 @@ struct question_t{
 	unsigned int difficulty;
 };
 
-struct question_t *sv_10q(struct question_t *arr[10]){
+struct question_t rquestion_f(){
+    
+    FILE *fp;
+    fp=fopen(fname,"rb");
+    struct question_t q;
+    int sizef = size_of_file();
+    int rng = rand() % sizef;
+
+    int j;
+    for(j = 0; j != rng;j++){
+        fread(&q,sizeof(q),1,fp);
+    };
+    return q;
+}
+
+//saving random 10 questions
+struct question_t *sv_10q(){
     FILE *fp;
     
-    struct question_t *q;
 
+    struct question_t *fq;
+    struct question_t arr[10];
+    
     fp=fopen(fname,"rb");
     
-    for(int i = 0; i < 10 ; i++){
+    
+    
+    for(int i = 0; i < 10 ; ){
         
-        fread(&q,sizeof(q),1,fp);
-        arr[i] = q;
-        //printf("%s",arr[i]->question);
+        
+        struct question_t rq = rquestion_f();
+        
+        if(i < 3){
+            if(rq.difficulty == 0){
+                arr[i] = rq;
+                i++;
+            };
+        };
+                
+        
+        if(i >= 3 && i <= 5){
+            if(rq.difficulty == 1){
+                arr[i] = rq;
+                i++;
+            };
+        };
+        if(i >= 6 && i <= 9){
+            if(rq.difficulty == 2){
+                arr[i] = rq;
+                i++;
+            };
+        };
     };
+        
+    fq = &arr[0];
     
     fclose(fp);
     
-    return *arr;
+    return fq;
+        
+
     
+}
+
+int size_of_file(){
+    
+    FILE *fp;
+    int size = 0;
+    struct question_t q;
+    
+    fp = fopen(fname,"rb");
+    while(1){
+        fread(&q,sizeof(q),1,fp);
+        size++;
+        if(feof(fp))
+            break;
+    };
+    fclose(fp);
+    
+    return size - 1;
 }
 
 void display()
@@ -114,24 +178,34 @@ void append(struct question_t *questions, int size){
 
 
 int main(){
+    
+
 	struct question_t questions[] = {
-	{"VUPROS1", "OTGOVOR1A", "ORGOVOR1B", "OTGOVOR1G", "ORGOVOR1D", 2,9},
-	{"VUPROS2", "OTGOVOR2A", "ORGOVOR2B", "OTGOVOR2G", "ORGOVOR2D", 1,2},	
-	{"VUPROS3", "OTGOVOR3A", "ORGOVOR3B", "OTGOVOR3G", "ORGOVOR3D", 4,1},
-	{"VUPROS4", "OTGOVOR4A", "ORGOVOR4B", "OTGOVOR4G", "ORGOVOR4D", 3,4},	
-	{"VUPROS5", "OTGOVOR5A", "ORGOVOR5B", "OTGOVOR5G", "ORGOVOR5D", 2,10},
-	{"VUPROS6", "OTGOVOR6A", "ORGOVOR6B", "OTGOVOR6G", "ORGOVOR6D", 1,6},	
-	{"VUPROS7", "OTGOVOR7A", "ORGOVOR7B", "OTGOVOR7G", "ORGOVOR7D", 4,3},
-	{"VUPROS8", "OTGOVOR8A", "ORGOVOR8B", "OTGOVOR8G", "ORGOVOR8D", 1,7},	
-	{"VUPROS9", "OTGOVOR9A", "ORGOVOR9B", "OTGOVOR9G", "ORGOVOR9D", 2,8},
-	{"VUPROS10", "OTGOVOR10A", "ORGOVOR10B", "OTGOVOR10G", "ORGOVOR10D", 3,5},	
-	{"VUPROS11", "OTGOVOR11A", "ORGOVOR11B", "OTGOVOR11G", "ORGOVOR11D", 3,3},
-	{"VUPROS12", "OTGOVOR12A", "ORGOVOR12B", "OTGOVOR12G", "ORGOVOR12D", 1,10},	
+	{"VUPROS1", "OTGOVOR1A", "ORGOVOR1B", "OTGOVOR1G", "ORGOVOR1D", 2,0},
+	{"VUPROS2", "OTGOVOR2A", "ORGOVOR2B", "OTGOVOR2G", "ORGOVOR2D", 1,1},	
+	{"VUPROS3", "OTGOVOR3A", "ORGOVOR3B", "OTGOVOR3G", "ORGOVOR3D", 4,2},
+	{"VUPROS4", "OTGOVOR4A", "ORGOVOR4B", "OTGOVOR4G", "ORGOVOR4D", 3,1},	
+	{"VUPROS5", "OTGOVOR5A", "ORGOVOR5B", "OTGOVOR5G", "ORGOVOR5D", 2,2},
+	{"VUPROS6", "OTGOVOR6A", "ORGOVOR6B", "OTGOVOR6G", "ORGOVOR6D", 1,0},	
+	{"VUPROS7", "OTGOVOR7A", "ORGOVOR7B", "OTGOVOR7G", "ORGOVOR7D", 4,2},
+	{"VUPROS8", "OTGOVOR8A", "ORGOVOR8B", "OTGOVOR8G", "ORGOVOR8D", 1,1},	
+	{"VUPROS9", "OTGOVOR9A", "ORGOVOR9B", "OTGOVOR9G", "ORGOVOR9D", 2,2},
+	{"VUPROS10", "OTGOVOR10A", "ORGOVOR10B", "OTGOVOR10G", "ORGOVOR10D", 3,2},	
+	{"VUPROS11", "OTGOVOR11A", "ORGOVOR11B", "OTGOVOR11G", "ORGOVOR11D", 3,0},
+	{"VUPROS12", "OTGOVOR12A", "ORGOVOR12B", "OTGOVOR12G", "ORGOVOR12D", 1,1},
+    {"VUPROS13", "OTGOVOR13A", "ORGOVOR13B", "OTGOVOR13G", "ORGOVOR13D", 1,1},
 	};
 	append(questions, sizeof(questions)/sizeof(questions[0]));
 	display();
-}
+    
+    struct question_t *questions10 = sv_10q();
+    
+    for(int i = 0; i < 10;i++){
+        printf("%s\n",(questions10+i)->question);
+    }
 
+
+}
 
 
 
